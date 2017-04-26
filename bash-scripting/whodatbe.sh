0963872df6bba6c4e77bfd0016b3e90da92e02d6
bj bautista <djbrigidope@bjs-MacBook-Pro.local>
2017-04-26 11:07:25 -0700

    putting command-line solutions into own .sh files

---
+++ bash-scripting/whodatbe.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Ask the user for their name
+echo Ayy, who dat be?
+read whoitbe
+echo It\'s nice to meet you $whoitbe
---
+++ command-line/3_cancelsercise.sh
@@ -0,0 +1,3 @@
+# Execute the command to sleep for 5000 seconds, realize that’s well over an hour, and then use the instructions from Box 4 to get out of trouble.
+$ sleep 5000
+^C
