21c5ebb0e1f43e4aab6a98d64dd480b217b8a3e1
bj bautista <emailme@djbrigidope.com>
2017-04-28 10:38:56 -0700

    some bash script tho

--- bash-scripting/1_firstcommand.sh
+++ bash-scripting/1_firstcommand.sh
@@ -1,3 +1,5 @@
 # Write a command that prints out the string “hello, world”.
 # Extra credit: As in Listing 1, do it two different ways,
 # both with and without using quotation marks.
+echo '"Hello World"'
+echo Hello World
